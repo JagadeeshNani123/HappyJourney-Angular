17f6dd6b8eb7eb690a0d34976a35b283d324a11d
JAGADEESH <virupakshijagadeeshreddyv@gmail.com>
2023-07-22 23:24:27 +0530

    Angular changes V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { SigninComponent } from './components/reservation/signin/signin.component';
+import { SignupComponent } from './components/reservation/signup/signup.component';
+import { DashboardComponent } from './components/reservation/dashboard/dashboard.component';
+import { HistoryComponent } from './components/reservation/history/history.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent,
+    SigninComponent,
+    SignupComponent,
+    DashboardComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
